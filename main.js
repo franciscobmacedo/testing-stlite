24ef903181d0d10f37d2acd2ec515384173c10b1
franciscobmacedo <franciscovcbm@gmail.com>
2023-05-05 15:35:40 +0100

    add base frontend

---
+++ main.js
@@ -0,0 +1,3 @@
+fetch('manifest.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
--- main.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write('hello from main!')
-
-if st.button('click here'):
-    st.write('hello again!')
