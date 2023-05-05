124c7121e670ea58b18a53af9319b899b01a43e8
franciscobmacedo <franciscovcbm@gmail.com>
2023-05-05 15:48:23 +0100

    add list apps functionality

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-fetch('manifest.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+function createAppList(arr) {
+  const list = document.getElementById("apps-container");
+  for (const entry of arr) {
+    const item = document.createElement("li");
+    const anchor = document.createElement("a");
+    const appName = entry.app.replace(/^.*[\\\/]/, "");
+    const scriptUrl = encodeURIComponent(entry.app);
+    const packages = entry.requirements.join("&req=");
+    const url = `https://share.stlite.net/#url=${scriptUrl}?req=${packages}`;
+    anchor.href = url;
+    anchor.textContent = appName;
+    anchor.target = "_blank";
+    item.appendChild(anchor);
+    list.appendChild(item);
+  }
+  return list;
+}
+fetch("manifest.json")
+  .then((response) => response.json())
+  .then((json) => createAppList(json));
