b439a22f6dbb1e2f3c65aa4de03dec0e5760a5b7
franciscobmacedo <franciscovcbm@gmail.com>
2023-05-05 16:46:32 +0100

    updateactions

--- build_apps_manifest.py
+++ build_apps_manifest.py
@@ -8,16 +8,21 @@ manifest = []
 for app_name in os.listdir(apps_dir):
     app_path = os.path.join(apps_dir, app_name)
     if os.path.isdir(app_path):
-        app_name = app_path.split('/')[-1].replace('_', ' ').capitalize()
-        app_file = os.path.join(app_path, "app.py")
-        if os.path.exists(app_file):
+        app_name = app_path.split("/")[-1].replace("_", " ").capitalize()
+        app_files = os.listdir(app_path)
+        python_files = [file for file in app_files if file.endswith("py")]
+
+        if python_files:
+            app_file = os.path.join(app_path, python_files[0])
             requirements_file = os.path.join(app_path, "requirements.txt")
             if os.path.exists(requirements_file):
                 with open(requirements_file, "r") as f:
                     requirements = [line.strip() for line in f.readlines()]
             else:
                 requirements = []
-            manifest.append({"app": f"apps/{app_name}/app.py", "requirements": requirements, "name": app_name})
+            manifest.append(
+                {"app": app_file, "requirements": requirements, "name": app_name}
+            )
 
 with open("manifest.json", "w") as f:
     json.dump(manifest, f, indent=2)
